1163726b6650eb7ba94058b56b6dd21f0787e78b
unknown <unknown@example.com>
2021-01-27 10:17:11 +0800

    edit map

--- miniprogram/pages/map/map.js
+++ miniprogram/pages/map/map.js
@@ -5,21 +5,51 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    latitude: 40.040417,
+    longitude: 116.273514,
+    markers:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that = this;
+    wx.getLocation({
+      type: "wgs84",
+      success: function (res) {
+        var latitude = res.latitude;
+        var longitude = res.longitude;
+        //console.log("当前位置的经纬度为：", res.latitude, res.longitude);
+        that.setData({
+          latitude: res.latitude,
+          longitude: res.longitude,
+        })
+      }
+    })
   },
+
+  /**
+   * 地图放大缩小事件触发
+   * @param {*} e 
+   */
+  //bindregionchange(e) {
+  //  console.log('=bindregiοnchange=', e)
+  //},
+
+  /**
+   * 点击地图事件，有经纬度信息返回
+   * @param {*} e 
+   */
+  //bindtapMap(e) {
+  //  console.log('=bindtapMap=', e)
+  //},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    //this.mapCtx = wx.createMapContext('map')
   },
 
   /**
@@ -62,5 +92,13 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+
+  //悬浮按钮跳转到上传页面
+ 
+  adddetial: function () {
+    wx.redirectTo({
+     url: '../upload/upload'
+    })
+ },
 })
