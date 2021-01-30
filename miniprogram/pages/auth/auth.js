7be6a3347b72205c7b37f97670280760094db1f0
o0arya0o <10185102139@stu.ecnu.edu.cn>
2021-01-30 14:16:41 +0800

    edit home page

--- miniprogram/pages/auth/auth.js
+++ miniprogram/pages/auth/auth.js
@@ -1,4 +1,6 @@
 // miniprogram/pages/auth/auth.js
+const app=getApp()
+const db=wx.cloud.database()
 Page({
 
   /**
@@ -12,7 +14,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    
   },
 
   /**
@@ -62,5 +64,35 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  bindGetUserInfo: function (res) {
+    if (res.detail.userInfo) {
+      //授权成功后，检查用户的openid是否在数据库中
+      db.collection('user').where({  
+        _openid: app.globalData.openid
+      })
+      .get({
+        success: function(res){
+          if(!res.data.length){  //如果没有找到数据，则跳转到注册页面
+            wx.redirectTo({
+              url: '../../pages/register/register',
+            })
+          }
+          else{    //找到数据，跳转到主页
+            wx.switchTab({
+              url: "../../pages/map/map",
+            })
+          }
+        }
+      })
+    } else {
+      //用户按了拒绝按钮
+      wx.showModal({
+        title: '注意',
+        content: '您需要授权以继续使用本程序',
+        showCancel: false,
+        confirmText: '返回授权',
+      });
+    }
   }
 })
